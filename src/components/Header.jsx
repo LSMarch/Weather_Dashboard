4ecd67bf6db22938fdaca413ba28882d47e421d6
Libby Marchetti <lsmarchetti01@gmail.com>
2023-05-08 21:16:24 -0500

    removed uneeded files, added features and components dirs, added header and search components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav className="navbar bg-primary" data-bs-theme="dark">
+            <div className="container-fluid">
+                <span className="navbar-brand mb-0 h1">Weather Dash</span>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
