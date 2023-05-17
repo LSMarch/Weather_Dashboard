1423224f714f4237b5e5efff6144e0c59605d381
Libby Marchetti <lsmarchetti01@gmail.com>
2023-05-16 20:12:44 -0500

    added forecast component

--- src/features/weather/Weather.jsx
+++ src/features/weather/Weather.jsx
@@ -1,7 +1,7 @@
 import { selectCityData } from '../cities/citySlice';
 import { useSelector } from 'react-redux';
 import Current from './Current';
-import { format } from 'date-fns';
+import Forecast from './Forecast';
 
 const Weather = () => {
     const status = useSelector(state => state.city.status);
@@ -21,29 +21,14 @@ const Weather = () => {
         const forecast = data.forecast.forecastday.map(day => (
             <article key={day.date}>
                 <div className='card m-2 border-0'>
-                    <div className='card-body'>
-                        <p className='card-title text-center border-bottom'>{format(new Date(day.date), 'EEEE')}</p>
-                        <img className='custom__sm-card-img' src={day.day.condition.icon} alt={data.current.condition.text} />
-                        <p className='card-text text-center'>{day.day.condition.text}</p>
-                        <p className='card-text text-center'><strong>Low: </strong> {day.day.mintemp_f}</p>
-                        <p className='card-text text-center'><strong>High: </strong> {day.day.maxtemp_f}</p>
-                    </div>
-
+                    <Forecast data={day} />
                 </div>
-
             </article>
         ))
         content = (
             <main className='d-flex flex-column mt-2'>
                 <div className='card '>
                     <Current data={data} />
-                    {/* <div className='card-body '>
-                        <h2 className='card-title text-center'>Currently in {data.location.name}</h2>
-                        <img className='custom__lg-card-img' src={data.current.condition.icon} alt={data.current.condition.text}></img>
-                        <p className='card-text text-center'>{data.current.condition.text}</p>
-                        <p className='text-center'>Temperature: {data.current.temp_f} F</p>
-                        <p className='text-center'>Feels Like: {data.current.feelslike_f}</p>
-                    </div> */}
                 </div>
                 <div className='card mt-4 '>
                     <h3 className='card-title text-center mt-1'>Forecast</h3>
